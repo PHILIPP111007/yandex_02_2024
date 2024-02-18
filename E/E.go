6031fb769b21cd34b31d7fc63fcfc9a5ee1d2719
Philipp Roshchin <r.phil@yandex.ru>
2024-02-18 18:30:24 +0300

    init

--- E/E.go
+++ E/E.go
@@ -14,47 +14,17 @@ import (
 	"github.com/adrg/strutil/metrics"
 )
 
-type Container struct {
-	num atomic.Uint64
-}
-
 var wg sync.WaitGroup
 
-func _func(c *Container, result map[int]string, i int, query string, universities *[]string) {
-	defer wg.Done()
-
-	maxSimilarity := 0.0
-	bestMatch := -1
-	for j, university := range *universities {
-
-		similarity := strutil.Similarity(strings.ToLower(query),
-			strings.ToLower(university), metrics.NewLevenshtein())
-
-		if similarity > maxSimilarity {
-			maxSimilarity = similarity
-			bestMatch = j
-		}
-	}
-
-	result[i] = (*universities)[bestMatch]
-
-	c.num.Add(1) // increment atomic counter
-	if c.num.Load()%100 == 0 {
-		fmt.Println(c.num.Load())
-	}
-}
-
 func main() {
-	universitiesFile, err :=
-		os.Open("/Users/phil/Downloads/task_2/universities.txt")
+	universitiesFile, err := os.Open("/Users/phil/Downloads/task_2/universities.txt")
 	if err != nil {
 		fmt.Println("Error opening universities file:", err)
 		return
 	}
 	defer universitiesFile.Close()
 
-	queriesFile, err :=
-		os.Open("/Users/phil/Downloads/task_2/queries.txt")
+	queriesFile, err := os.Open("/Users/phil/Downloads/task_2/queries.txt")
 	if err != nil {
 		fmt.Println("Error opening queries file:", err)
 		return
@@ -77,9 +47,9 @@ func main() {
 
 	wg.Add(len(queries))
 	var result_map = make(map[int]string)
-	c := Container{}
+	var counter atomic.Uint64
 	for i := 0; i < len(queries); i++ {
-		_func(&c, result_map, i, queries[i], &universities)
+		_func(&counter, result_map, i, queries[i], &universities)
 	}
 	wg.Wait()
 
@@ -89,8 +59,7 @@ func main() {
 	}
 	sort.Ints(keys)
 
-	answerFile, err :=
-		os.Create("/Users/phil/Downloads/task_2/answerGo.txt")
+	answerFile, err := os.Create("/Users/phil/Downloads/task_2/answerGo.txt")
 	if err != nil {
 		fmt.Println("Error opening answer file:", err)
 		return
@@ -99,5 +68,29 @@ func main() {
 
 	for i := range keys {
 		answerFile.WriteString(result_map[i] + "\n")
+	}
+}
+
+func _func(counter *atomic.Uint64, result map[int]string, i int, query string, universities *[]string) {
+	defer wg.Done()
+
+	maxSimilarity := 0.0
+	bestMatch := -1
+	for j, university := range *universities {
+
+		similarity := strutil.Similarity(strings.ToLower(query),
+			strings.ToLower(university), metrics.NewLevenshtein())
+
+		if similarity > maxSimilarity {
+			maxSimilarity = similarity
+			bestMatch = j
+		}
+	}
+
+	result[i] = (*universities)[bestMatch]
+
+	counter.Add(1) // increment atomic counter
+	if counter.Load()%100 == 0 {
+		fmt.Println(counter.Load())
 	}
 }
